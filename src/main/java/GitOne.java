39a9297fc3ca5a427b6b0bca5320acb3b72989f6
Aleksandr <gvardianez@yandex.ru>
2021-10-14 20:22:57 +0300

    init 4

--- src/main/java/GitOne.java
+++ src/main/java/GitOne.java
@@ -1,7 +1,6 @@
 public class GitOne {
     public static void main(String[] args) {
         System.out.println("1212122");
-        System.out.println("dasdsadsad");
-        System.out.println("3213212");
+
     }
 }
